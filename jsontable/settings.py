c4e21ed4d277d72cc1656ad5467186d55b75a9ef
Kent Huang <kenthuang426@gmail.com>
2018-01-03 18:06:39 -0800

    referenced file path in views.py

--- jsontable/settings.py
+++ jsontable/settings.py
@@ -15,6 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+APP_DIR = os.path.join(BASE_DIR, "threatapp")
+
+THREAT_FILE = os.path.join(APP_DIR,"static","filesRecords","threat.txt")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
--- threatapp/views.py
+++ threatapp/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 
+import json
+
+import os
+from django.conf import settings
+
+
 
 
 def index(request, periodtype):
     """
     View function for home page of site.
     """
+    fh = open(settings.THREAT_FILE)
+
+    x = json.load(fh)
+
+    print(x)
 
     
     # Render the HTML template index.html with the data in the context variable
