eb7b6c0aef2aef35751cffef5c02f0fcd26588b2
Kent Huang <kenthuang426@gmail.com>
2018-01-05 01:21:35 -0800

    added backend url api route and frontend dropdown functionality

--- threatapp/views.py
+++ threatapp/views.py
@@ -12,6 +12,9 @@ from django.http import JsonResponse
 
 from django.views.decorators.cache import never_cache
 
+from datetime import datetime
+from datetime import timedelta
+
 @never_cache
 def index(request):
 
@@ -29,10 +32,11 @@ def getmeta(request, periodtype):
     with open(settings.THREAT_FILE, 'rb') as fh:
     	
     	data = fh.read()
+    	#convert from byte data to text
     	text = data.decode('utf-8')
     	# replace Unicode for the single left and right quote characters with the ACSII equivalent
     	text = text.replace(u"\u2018", "'").replace(u"\u2019", "'")
-    	#convert whitespace characters to a single space
+    	#convert whitespace characters(defined in string.whitespace) to a single space
     	text = text.translate(str.maketrans("\t\n\r\x0b\x0c", "     "))
 
     	#print(text)
@@ -42,14 +46,26 @@ def getmeta(request, periodtype):
 
     	#safely evaluate the string to python list
     	list_data = ast.literal_eval(text)
+    	print(list_data)
+
+    	cur_date = datetime.now()
+
+    	js_data = []
+    	if periodtype == "1" :
+    		start_week = cur_date - timedelta(7)
+    		js_data = list(filter(lambda record: datetime.strptime(record["date"], '%b %d, %Y %H:%M:%S') > start_week and datetime.strptime(record["date"], '%b %d, %Y %H:%M:%S') < cur_date, list_data))
+    	elif periodtype == "2" :
+    		start_four_weeks = cur_date - timedelta(28)
+    		js_data = list(filter(lambda record: datetime.strptime(record["date"], '%b %d, %Y %H:%M:%S') > start_four_weeks and datetime.strptime(record["date"], '%b %d, %Y %H:%M:%S') < cur_date, list_data))
+    	else:
+    		start_today = cur_date - timedelta(1)
+    		js_data = list(filter(lambda record: datetime.strptime(record["date"], '%b %d, %Y %H:%M:%S') > start_today and datetime.strptime(record["date"], '%b %d, %Y %H:%M:%S') < cur_date, list_data))
+    	#%b %d, %Y %H %M %S
+    	print(js_data)
+
+
+    	
 
 
     fh.closed
-    return JsonResponse(list_data, safe = False)
-    
-    # Render the HTML template index.html with the data in the context variable
-    # return render(
-    #     request,
-    #     'index.html',
-    #     context={'pt':periodtype, 'data' : obj},
-    # )
+    return JsonResponse(js_data, safe = False)
